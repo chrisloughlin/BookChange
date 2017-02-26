449c0e0b678062ac08e0cef47abb0f6e8efc4348
TedBausch <tedbausch@gmail.com>
2017-02-26 16:54:59 -0500

    started work on the login activity

--- app/src/main/java/com/example/bookchange/bookchange/LoginActivity.java
+++ app/src/main/java/com/example/bookchange/bookchange/LoginActivity.java
@@ -1,10 +1,27 @@
 package com.example.bookchange.bookchange;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by christopher on 2/26/17.
  */
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+
+    public void onLoginClicked (View button){
+
+    }
+
+    public void onCreateClicked (View button){
+
+    }
 }
