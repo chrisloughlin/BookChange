52d02efdc0676af7aee49cb63a2e0cbf995e2f9f
chrisloughlin <christopher.r.loughlin.17@dartmouth.edu>
2017-02-26 16:32:25 -0500

    Login and coming soon add

---
+++ app/src/main/java/com/example/bookchange/bookchange/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.bookchange.bookchange;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by christopher on 2/26/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
