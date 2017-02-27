97e517d70b5a3cec96cb9ba9c53109e638978108
TedBausch <tedbausch@gmail.com>
2017-02-26 21:03:49 -0500

    Made CourseSelect into fragment

--- app/src/main/java/com/example/bookchange/bookchange/CourseSelect.java
+++ app/src/main/java/com/example/bookchange/bookchange/CourseSelect.java
@@ -2,7 +2,10 @@ package com.example.bookchange.bookchange;
 
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,37 +16,33 @@ import java.util.ArrayList;
  * Created by Ted on 2/26/2017.
  */
 
-public class CourseSelect extends AppCompatActivity {
+public class CourseSelect extends Fragment {
     private int departmentInt;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.course_select);
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup paramsGroup, Bundle savedInstanceState){
+
+        View mView = inflater.inflate(R.layout.course_select, paramsGroup, false);
         // get the int for the selected department
-        if (savedInstanceState == null) {
-            Bundle extras = getIntent().getExtras();
-            if(extras == null) {
-               departmentInt = -1;
-            } else {
-                departmentInt = extras.getInt("department_int");
-            }
-        } else {
-            departmentInt = savedInstanceState.getInt("department_key");
-        }
-
+        departmentInt = getArguments().getInt("department_int");
         // Set the text for the title
-        TextView departmentTitle = (TextView) findViewById(R.id.title_department);
+        TextView departmentTitle = (TextView) mView.findViewById(R.id.title_department);
         Resources res = getResources();
         String[] departments = res.getStringArray(R.array.departments_array);
         departmentTitle.setText(departments[departmentInt]);
 
         // set up the listView
-        ListView coursesListView = (ListView) findViewById(R.id.list);
+        ListView coursesListView = (ListView) mView.findViewById(R.id.list);
         String[] courses = { "CS 1", "CS 10", "CS 30", "CS 50", "CS 65" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, courses);
         coursesListView.setAdapter(adapter);
+
+        return mView;
     }
 }
