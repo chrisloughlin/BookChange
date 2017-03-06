84237d1113b9951379fa65e7059e109eb03944e6
SachinVadodaria <sachin.m.vadodaria.18@dartmouth.edu>
2017-03-06 15:22:55 -0500

    subscr stuff

---
+++ app/src/main/java/com/example/bookchange/bookchange/SubscriptionsAdapter.java
@@ -0,0 +1,58 @@
+package com.example.bookchange.bookchange;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sachinvadodaria on 3/3/17.
+ */
+
+public class SubscriptionsAdapter extends ArrayAdapter {
+
+    public SubscriptionsAdapter(Context context, List<Subscription> values){
+//        android.R.layout.simple_list_item_1
+        super(context, R.layout.yoursubsfragment, android.R.id.text1, values);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parentViewGroup){
+        View view = super.getView(position, convertView, parentViewGroup);
+        TextView textView = (TextView) view.findViewById(android.R.id.text1);
+        Subscription entry = (Subscription) getItem(position);
+
+        ArrayList classes = entry.getClasses();
+        textView.setText("test");
+
+        return view;
+    }
+
+//    private final ArrayList</*ExerciseEntry*/> entries;
+//    private final ArrayList<String> subs;
+//
+//    public SubscriptionsAdapter(Context context, ArrayList<ExerciseEntry> data) {
+//        super(context, android.R.layout.simple_list_item_2, android.R.id.text1, data);
+//        subs = data;
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        View view = super.getView(position, convertView, parent);
+//        TextView tV1 = (TextView) view.findViewById(android.R.id.text1);
+//        TextView tV2 = (TextView) view.findViewById(android.R.id.text2);
+//
+//        String line1 = subs.get(position).getInputType() + ": " + entries.get(position).getActivityType() + ", "
+//                + HistoryFragment.convertDate(subs.get(position).getDateAndTime().getTimeInMillis(), "hh:mm:ss MMM dd yyyy");
+//        String line2 = entries.get(position).getDistance() + " Miles, " + subs.get(position).getDuration() + "secs";
+//
+//        tV1.setText(line1);
+//        tV2.setText(line2);
+//        return view;
+//    }
+
+}
