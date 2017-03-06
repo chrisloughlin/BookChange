1612fda33c2f097cc995c4a99ca0ecd8393e4eb4
SachinVadodaria <sachin.m.vadodaria.18@dartmouth.edu>
2017-03-06 16:44:21 -0500

    subscr stuff

--- app/src/main/java/com/example/bookchange/bookchange/YourSubscriptions.java
+++ app/src/main/java/com/example/bookchange/bookchange/YourSubscriptions.java
@@ -70,35 +70,32 @@ public class YourSubscriptions extends Fragment {
 //        dataSource.open();
 //        child("users").child(userId).
 //        subscriptions.clear();
+        final SubscriptionsAdapter adapter = new SubscriptionsAdapter(getActivity(), subscriptions);
+
         mDatabase.child("users").child(userId).child("subscriptions").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 subscriptions.add(dataSnapshot.getValue(Subscription.class));
+                adapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
                 subscriptions.remove(dataSnapshot.getValue(Subscription.class));
+                adapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
+            public void onCancelled(DatabaseError databaseError) {}
         });
 //        final ArrayList<BookListing> listings = dataSource.fetchEntriesByPosterUsername(account.getAccountName());
 //        dataSource.close();
-        SubscriptionsAdapter adapter = new SubscriptionsAdapter(getContext(), subscriptions);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
