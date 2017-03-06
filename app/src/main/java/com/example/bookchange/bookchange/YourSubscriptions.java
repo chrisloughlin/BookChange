42764fdc5bf077fd5a67b0e54f494b9445d00bdf
SachinVadodaria <sachin.m.vadodaria.18@dartmouth.edu>
2017-03-06 16:40:30 -0500

    subscr stuff

--- app/src/main/java/com/example/bookchange/bookchange/YourSubscriptions.java
+++ app/src/main/java/com/example/bookchange/bookchange/YourSubscriptions.java
@@ -3,6 +3,7 @@ package com.example.bookchange.bookchange;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -59,6 +60,8 @@ public class YourSubscriptions extends Fragment {
 //        MainActivity activity = (MainActivity) getActivity();
 //        BookchangeAccount account = activity.getAccount();
 
+        Log.d("subs", "subs");
+        Log.d("uID", userId);
         subscriptions = new ArrayList<>();
 
         // set up the listView
@@ -66,7 +69,7 @@ public class YourSubscriptions extends Fragment {
 //        BookListingDataSource dataSource = new BookListingDataSource(getActivity());
 //        dataSource.open();
 //        child("users").child(userId).
-        subscriptions.clear();
+//        subscriptions.clear();
         mDatabase.child("users").child(userId).child("subscriptions").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
